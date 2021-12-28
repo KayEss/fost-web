257fcfdbde537114c22fa3e6a98d3e4900fabdcd
Kirit Sælensminde <kirit@kirit.com>
2021-12-28 12:21:10 +0700

    Add new `fostgres.middleware.response` to set response headers

---
+++ Cpp/fost-urlhandler/middleware.response.cpp
@@ -0,0 +1,28 @@
+#include "fost-urlhandler.hpp"
+#include <fost/urlhandler.hpp>
+
+
+namespace {
+
+
+    const class response_middleware : public fostlib::urlhandler::view {
+      public:
+        response_middleware() : view("fost.middleware.response") {}
+
+        std::pair<boost::shared_ptr<fostlib::mime>, int> operator()(
+                const fostlib::json &configuration,
+                const fostlib::string &path,
+                fostlib::http::server::request &req,
+                const fostlib::host &h) const {
+            auto response = execute(configuration, path, req, h);
+            for (auto h = configuration["headers"].begin();
+                 h != configuration["headers"].end(); ++h) {
+                response.first->headers().set(
+                        fostlib::coerce<fostlib::string>(h.key()), *h);
+            }
+            return response;
+        }
+    } c_middleware_request;
+
+
+}
