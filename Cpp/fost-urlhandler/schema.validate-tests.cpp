0983d7a03c9745ffda476d5d5c8d9f00daf33958
Kirit Sælensminde <kirit@felspar.com>
2026-01-17 14:02:50 +0700

    Remove dead copyrights

--- Cpp/fost-urlhandler/schema.validate-tests.cpp
+++ Cpp/fost-urlhandler/schema.validate-tests.cpp
@@ -1,11 +1,3 @@
-/**
-    Copyright 2018-2020 Red Anchor Trading Co. Ltd.
-
-    Distributed under the Boost Software License, Version 1.0.
-    See <http://www.boost.org/LICENSE_1_0.txt>
- */
-
-
 #include <fost/push_back>
 #include <fost/urlhandler>
 #include <fost/test>
